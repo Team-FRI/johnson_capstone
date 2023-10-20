70381709ec054309945750309d93f4a8fb480772
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2023-10-20 14:18:51 -0400

    test

--- data_testing/data_testing_1.R
+++ data_testing/data_testing_1.R
@@ -4,3 +4,11 @@ setwd("C:/GitHub/johnson_capstone/data_testing")
 
 getwd()
 dir()
+
+#Download Read xl package so R can read in your data
+install.packages("readxl")
+library(readxl)
+
+#Read in water temp data for Longterm Loyalsock Project 
+
+Data1.xlsx.tibble <- read_excel("Data.xlsx")
