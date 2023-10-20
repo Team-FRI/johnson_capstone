db35dbf57cb4050b7606ccc8a8cb6edf65e1cc05
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2023-10-20 19:56:33 -0400

    crap

--- data_testing/data_testing_1.R
+++ data_testing/data_testing_1.R
@@ -5,16 +5,32 @@ setwd("C:/GitHub/johnson_capstone/data_testing")
 getwd()
 dir()
 
-#Download Read xl package so R can read in your data
+$#First we will need to download the readxl package since one out of the three data sets is a .xlsx
+
 install.packages("readxl")
 library(readxl)
 
-#Read in water temp data for Longterm Loyalsock Project 
+#So now I will read in the .xlsx file "Data.xlsx"
 
-Data1.xlsx.tibble <- read_excel("Data.xlsx")
+Brunnerdale_F <- read_excel("FishRecords.xlsx")
 
-Data1.xlsx.tibble <- read_excel("Brunnerdale_Run_Stream_X_QC.xlsx", True)
+#Beware that using the read_excel function turns the dara into a "tibble format". 
+#In order to fix this, I need to do the as.data.frame function
 
-Data1 <- read.csv("Brunnerdale_Run_Stream_X_QC.csv")
+Brunnerdale_Fish_Records <- as.data.frame(Data1.xlsx.tibble)
 
-Brunnerdale_Run_Land_X_QC
+
+#This is not an excel file, so we don't need to download the read excell package.
+
+#Read in water temp data for Long term Loyalsock Project at the Brunnerdale location.  
+
+Brunnerdale_Stream_Temp <- read.csv("Brunnerdale_Run_Stream_X_QC.csv")
+
+Brunnerdale_Land_Temp <- read.csv("Brunnerdale_Run_Land_X_QC.csv")
+
+Brunnerdale_Fish_Records <- read.csv("FishRecords.csv")
+
+#Now that both CSV's are red in, we need  a unique identifier that brings the two data sets together. 
+#Luckily, both the temperature and fish data was taken in the same location (Brunnerdale Run). 
+#However, these are not currennlty columns in the data tables, so I need to add a column.
+#
