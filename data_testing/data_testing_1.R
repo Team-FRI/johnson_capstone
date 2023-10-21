108c47b0cf85c0ec133467753b1c035fa1037d0c
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2023-10-20 20:35:15 -0400

    ugh

--- data_testing/data_testing_1.R
+++ data_testing/data_testing_1.R
@@ -5,22 +5,21 @@ setwd("C:/GitHub/johnson_capstone/data_testing")
 getwd()
 dir()
 
-$#First we will need to download the readxl package since one out of the three data sets is a .xlsx
+#First we will need to download the readxl package since one out of the three data sets is a .xlsx
 
 install.packages("readxl")
 library(readxl)
 
 #So now I will read in the .xlsx file "Data.xlsx"
 
-Brunnerdale_F <- read_excel("FishRecords.xlsx")
+Brunnerdale_Fish_Records <- read_excel("FishRecords.xlsx")
 
-#Beware that using the read_excel function turns the dara into a "tibble format". 
+#Beware that using the read_excel function turns the data into a "tibble format". 
 #In order to fix this, I need to do the as.data.frame function
 
-Brunnerdale_Fish_Records <- as.data.frame(Data1.xlsx.tibble)
+Brunnerdale_Fish_Records1 <- as.data.frame(Brunnerdale_Fish_Records)
 
-
-#This is not an excel file, so we don't need to download the read excell package.
+#This other 2 datasets we need to read in are not excell files, their CSV's.
 
 #Read in water temp data for Long term Loyalsock Project at the Brunnerdale location.  
 
@@ -28,9 +27,10 @@ Brunnerdale_Stream_Temp <- read.csv("Brunnerdale_Run_Stream_X_QC.csv")
 
 Brunnerdale_Land_Temp <- read.csv("Brunnerdale_Run_Land_X_QC.csv")
 
-Brunnerdale_Fish_Records <- read.csv("FishRecords.csv")
-
 #Now that both CSV's are red in, we need  a unique identifier that brings the two data sets together. 
 #Luckily, both the temperature and fish data was taken in the same location (Brunnerdale Run). 
-#However, these are not currennlty columns in the data tables, so I need to add a column.
-#
+#However, these are not currency columns in the data tables, so I need to add a column.
+#To do that, I will need to R bind, but first I will need to remove the columns from the Brunnerdale_Fish_Records_1 file that I don't want to use.
+
+Brunnerdale_Fish_Records2 <- Brunnerdale_Fish_Records1[,c(-3:-7, -10:-12, -14:-16)]
+
