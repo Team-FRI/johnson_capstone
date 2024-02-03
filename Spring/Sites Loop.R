cfd9f3a7ace4ea0a8830da51dcb638a9a7eb44bc
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2024-02-03 17:20:32 -0500

    pause .5.30

--- Spring/Sites Loop.R
+++ Spring/Sites Loop.R
@@ -12,7 +12,7 @@ library(readxl)
 AllFishRec <- read_excel ("All_FishRecords.xlsx")
 
 
-#To build a loopp for all the sites, its easier to chose one site and mess around with that first insetad of trying all the sites at once!
+#To build a loopp for all the sites, its easier to chose one site and mess around with that first instead of trying all the sites at once!
 #Also to make things easier, lets choose a site that has been quality control checked for temp data!
 #Painter run aka Painter.LittleBear will be our first choice and its already quality control checked. 
 
@@ -26,7 +26,7 @@ Painter_ST <- read.csv("Painter_Stream_X_QC.csv")
 install.packages("dplR")
 library(dplyr)
 
-By.Site <- filter(AllFishRec, waterName == "Painter.Run") 
+By.Site <- filter(AllFishRec, waterName == "Painter.Run")
 
 By.Species_BKT <- filter(By.Site, Species == "Brook Trout")
 
@@ -60,7 +60,7 @@ AllFishSurvey <- read_excel("All_FishSurvey.xlsx")
 #Bellow is the one you want and thhat works for pulling event info for certain sites 
 
 #In order to calculate CPUE for Painter Run, I need to pull dates, widths, and shock times from the AllFishSurvey data frame. 
-PainterEventInfo <- subset(AllFishSurvey, AllFishSurvey$SiteCode == "Painter.LittleBear") 
+PainterEventInfo <- subset(AllFishSurvey, AllFishSurvey$SiteCode == "Painter.LittleBear")
 #Should I only use Painter.LittleBear as painter run or should i use other one as well like Painter.Elk , the UNT, etc???? Ask Matt next meeting 
 
 #Install the lubridate package to doubble chekc that the dates are in POSIXct and NOT character
@@ -81,6 +81,8 @@ PainterEventInfo1$Area <- PainterEventInfo1$Length * PainterEventInfo1$AveWid
 print(df)
 
 #Now we have our area column and need to calculate CPUE for Painter run 
+#We will do this for each year by its unique event code. 
+#First is 201107.Painter.LittleBear from 2011
 
 event_code <- "201107.Painter.LittleBear"
 
@@ -90,9 +92,183 @@ TwentyEleven_Area <- subset(PainterEventInfo1, EventCode == event_code)
 
 TwentyEleven$Area <- TwentyEleven_Area$Area[1]
 
-TwentyEleven$BTCount <- c(1,1,1,1,1,1,1,1,1,1,1)
+TwentyEleven$BTCount <- rep(1, times = 11)
 
 TwentyEleven$CPUE <- sum(TwentyEleven$BTCount) / TwentyEleven$Area
 
 #CHECK IF CPUE SHOULD BE A PERCENTAGE OR NOT. That should be an easy fix though. 
 #Now repeat for each year site was sampled. 
+
+#Next is  201206.Painter.LittleBear from 2012
+
+event_code <- "201206.Painter.LittleBear"
+
+TwentyTwelve <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentyTwelve_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentyTwelve$Area <- TwentyEleven_Area$Area[1]
+
+TwentyTwelve$BTCount <-rep(1, times = 35)
+
+TwentyTwelve$CPUE <- sum(TwentyTwelve$BTCount) / TwentyTwelve$Area
+
+#Next is  201306.Painter.LittleBear from 2013
+
+event_code <- "201306.Painter.LittleBear"
+
+TwentyThirteen <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentyThirteen_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentyThirteen$Area <- TwentyThirteen_Area$Area[1]
+
+TwentyThirteen$BTCount <-rep(1, times = 43)
+
+TwentyThirteen$CPUE <- sum(TwentyThirteen$BTCount) / TwentyThirteen$Area
+
+#Next is 201406.Painter.LittleBear from 2014
+
+event_code <- "201406.Painter.LittleBear"
+
+TwentyFourteen <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentyFourteen_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentyFourteen$Area <- TwentyFourteen_Area$Area[1]
+
+TwentyFourteen$BTCount <-rep(1, times = 10)
+
+TwentyFourteen$CPUE <- sum(TwentyFourteen$BTCount) / TwentyFourteen$Area
+
+#Next is 201506.Painter.LittleBear from 2015
+
+event_code <- "201506.Painter.LittleBear"
+
+TwentyFifteen <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentyFifteen_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentyFifteen$Area <- TwentyFifteen_Area$Area[1]
+
+TwentyFifteen$BTCount <-rep(1, times = 22)
+
+TwentyFifteen$CPUE <- sum(TwentyFifteen$BTCount) / TwentyFifteen$Area
+
+#Next is 201606.Painter.LittleBear from 2016
+
+event_code <- "201606.Painter.LittleBear"
+
+TwentySixteen <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentySixteen_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentySixteen$Area <- TwentySixteen_Area$Area[1]
+
+TwentySixteen$BTCount <-rep(1, times = 24)
+
+TwentySixteen$CPUE <- sum(TwentySixteen$BTCount) / TwentySixteen$Area
+
+#Next is 201706.Painter.LittleBear from 2017
+
+event_code <- "201706.Painter.LittleBear"
+
+TwentySeventeen <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentySeventeen_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentySeventeen$Area <- TwentySeventeen_Area$Area[1]
+
+TwentySeventeen$BTCount <-rep(1, times = 12)
+
+TwentySeventeen$CPUE <- sum(TwentySeventeen$BTCount) / TwentySeventeen$Area
+
+#Next is 201806.Painter.LittleBear from 2018
+
+event_code <- "201806.Painter.LittleBear"
+
+TwentyEighteen <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentyEighteen_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentyEighteen$Area <- TwentyEighteen_Area$Area[1]
+
+TwentyEighteen$BTCount <-rep(1, times = 15)
+
+TwentyEighteen$CPUE <- sum(TwentyEighteen$BTCount) / TwentyEighteen$Area
+
+#Next is 201906.Painter.LittleBear from 2019
+
+event_code <- "201906.Painter.LittleBear"
+
+TwentyNineteen <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentyNineteen_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentyNineteen$Area <- TwentyNineteen_Area$Area[1]
+
+TwentyNineteen$BTCount <-rep(1, times = 31)
+
+TwentyNineteen$CPUE <- sum(TwentyNineteen$BTCount) / TwentyNineteen$Area
+
+#Next is 202006.Painter.LittleBear from 2020
+
+event_code <- "202006.Painter.LittleBear"
+
+TwentyTwenty <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentyTwenty_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentyTwenty$Area <- TwentyTwenty_Area$Area[1]
+
+TwentyTwenty$BTCount <-rep(1, times = 10)
+
+TwentyTwenty$CPUE <- sum(TwentyTwenty$BTCount) / TwentyTwenty$Area
+
+#Next is 202106.Painter.LittleBear from 2021
+
+event_code <- "202106.Painter.LittleBear"
+
+TwentyTwentyOne <- By.Species_BKT[By.Species_BKT$EventCode == event_code, ]
+
+TwentyTwentyOne_Area <- subset(PainterEventInfo1, EventCode == event_code)
+
+TwentyTwentyOne$Area <- TwentyTwentyOne_Area$Area[1]
+
+TwentyTwentyOne$BTCount <-rep(1, times = 10)
+
+TwentyTwentyOne$CPUE <- sum(TwentyTwentyOne$BTCount) / TwentyTwentyOne$Area
+
+#2021 is the onlyy year I can't get to work. I think its because the water name in PainterEventsInfo 1 is "Painter Run" and not "Painter.Run" like all the other years are. 
+#I treid multiple ways of fixing this and nothing seems to work. 
+
+#Making Ratios 
+
+install.packages("tidyr")
+library(tidyr)
+
+AgeClassCounts <- Fish.Age.Site %>%
+  group_by(EventCode, AgeClass) %>%
+  summarize(count = n())
+
+# Pivot the data to have YOY and adult as columns
+ratio_data <- AgeClassCounts %>%
+  pivot_wider(names_from = AgeClass, values_from = count, values_fill = 0)
+
+# Calculate the ratio
+ratio_data <- ratio_data %>%
+  mutate(Ratio_YOY_to_Adult = YOY / Adult)
+
+#Now lets calculate biomass 
+
+Biomass <- data.frame("2011" = NA, "2012" = NA, "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021")
+                      
+Biomass$X2011 <- sum(TwentyEleven$Wt_g)/ (TwentyEleven$Area[1])
+
+Biomass$X2011 <- sum(TwentyEleven$Wt_g) / TwentyEleven$Area
+
+Biomass$X2011 <- sum(TwentyEleven$Wt_g) / TwentyEleven$Area
+
+sum(TwentyEleven$Wt_g) / TwentyEleven$Area[1]
+
+Biomass <- data.frame("2011" = NA)
