7e04465ea827b9d01482d5413c50136725943214
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2024-02-20 14:47:45 -0500

    afternoon work 2-20-24

--- Spring/10 Spring stats.R
+++ Spring/10 Spring stats.R
@@ -62,3 +62,87 @@ summary(cpueC_AvgMax)
 plot(cpueC_AvgMax)
 
 #P value = 0.01295. Less than 0.05 so significant 
+
+#Now lets move onto a new Y variable: CPUE_Biomass
+#First lets try CPUE_Biomass against Highest Temp/ Year Month 
+
+cpueB_HighT <- lm(CPUE_Biomass ~ Highest_Temperature_C, data = Data_merge)
+summary(cpueB_HighT)
+
+plot(cpueC_HighT)
+
+# P value = 0.008814. Less than 0.05 so significant!
+
+#Next lets do CPUE_Biomass against Lowest Temp/ Year Month 
+
+cpueB_LowT <- lm(CPUE_Biomass ~ Lowest_Temperature_C, data = Data_merge)
+summary(cpueB_LowT)
+
+plot(cpueC_LowT)
+
+#P value is  0.2367. Greater then 0.05 so not significant
+
+#Next lets do CPUE_Biomass against AvgMin
+
+cpueB_AvgMin <- lm(CPUE_Biomass ~ AvgMin, data = Data_merge)
+summary(cpueB_AvgMin)
+
+plot(cpueB_AvgMin)
+
+#P value = 0.1848. Greater than 0.05 so not significant 
+
+#Lastly for this Y variable, lets do CPUE_Biomass against AvgMax
+
+cpueB_AvgMax <- lm(CPUE_Biomass ~ AvgMax, data = Data_merge)
+summary(cpueB_AvgMax)
+
+plot(cpueB_AvgMax)
+
+#P value = 0.02139. Less than  0.05 so  significant!
+
+#Now lets move onto our third and final response variable, RatAY
+#First lets do Rat_AY against Highest_Temperature_C
+
+RatAY_HighT <- lm(RatAY ~ Highest_Temperature_C, data = Data_merge)
+summary(RatAY_HighT)
+
+plot(RatAY_HighT)
+
+#P Value = 0.5589. Greater than 0.05 so not significant 
+
+#Next lets do RatAY against Lowest_Temperature_C
+
+RatAY_LowT <- lm(RatAY ~ Lowest_Temperature_C, data = Data_merge)
+summary(RatAY_LowT)
+
+plot(RatAY_LowT)
+
+#P value = 0.6892. Greater than 0.05 so not significant 
+
+#Next Lets do RatAY against AvgMin
+
+RatAy_AvgMin <- lm(RatAY ~ AvgMin, data = Data_merge)
+summary(RatAy_AvgMin)
+
+plot(RatAy_AvgMin)
+
+#P value = 0.8367. Greater than 0.05 so not significant
+
+#Laslty for this Y variable, lets do RatAy against AvgMax 
+
+RatAy_AvgMax <- lm(RatAY ~ AvgMax, data = Data_merge)
+summary(RatAy_AvgMax)
+
+plot(RatAy_AvgMax)
+
+#P value = 0.7135. Greater than 0.05 so not significant 
+
+#The next step is going to be trying to make a GLM. 
+
+GLM_CPUEC_HighT <- glm(CPUE_Count ~ Highest_Temperature_C, data = Data_merge, family = binomial())
+
+GLM_CPUEC_HighT <- glm(CPUE_Count ~ Highest_Temperature_C, data = Data_merge, family = gaussian()
+
+
+
+cpue_BTestAvgMax <- lm(BKTVar$CPUE_Biomass ~ STPred$AvgMax)
