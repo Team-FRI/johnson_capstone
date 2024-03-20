b614b738ae969fc481cd558ccc22bd4081dc12c5
Sara Ashcraft <ashcraft@susqu.edu>
2024-03-20 10:15:24 -0400

    update
    updating code to prep for modeling

--- Spring/01 LTLoyalsockDBFilePrep.R
+++ Spring/01 LTLoyalsockDBFilePrep.R
@@ -88,8 +88,65 @@ EventsLoyal<-Events%>%
   arrange(SiteCode)
 
 EventsLoyal#306 x 3
+#    Time had only 9 surveys out of 306 with a time recorded - removed from data prep.
+#QC- that all events make sense as LTL.
+EvLoySum<-EventsLoyal%>%
+  group_by(SiteCode)%>%
+  summarise(
+    LoyalEventCount = n()
+  )
+EvLoySum
+# 24 of 30 sites have 10 events.
+# Sites with 11 events need investigated: Coal, DryHB, Granddad, Painter, Red, Rock
+#EventsInv<-EventsLoyal%>%
+#  filter(SiteCode =="Coal.UpperLoyalsock"|SiteCode =="DryHB.Hoagland"|SiteCode =="Grandad.Hessler"|
+#           SiteCode =="Painter.LittleBear"|SiteCode =="Red.LittleBear"|SiteCode =="Rock.LittleLoyalsock")%>%
+#  arrange(SiteCode,EventCode)
+#EventsInv# all sites surveyed in 2021 - an additional year after all other sites labeled Loyalsock.
 
-#Update Site Names to match Temperature files
+
+#Reduce Sites to only Sites categorized as Loyalsock
+SitesLoyal<-left_join(EvLoySum,Sites,by="SiteCode")
+SitesLoyalU<-SitesLoyal[, c(1,4,5,2,6,7)]%>%
+  arrange(SiteCode)
+SitesLoyalU# 30 x 6
+
+#Update SiteCodes in Sites df to match Temperature files
+SitesLoyalU$SiteCode[1]<-"Bear.Loyalsock"
+SitesLoyalU$SiteCode[2]<-"Brunnerdale.Ogdonia"
+SitesLoyalU$SiteCode[3]<-"Coal.Loyalsock"
+SitesLoyalU$SiteCode[4]<-"Conklin.Mill"
+SitesLoyalU$SiteCode[5]<-"Dry.Hoagland"
+SitesLoyalU$SiteCode[6]<-"Dry.Loyalsock"
+SitesLoyalU$SiteCode[7]<-"Dutchman.Loyalsock"
+SitesLoyalU$SiteCode[8]<-"Ellis.Loyalsock"
+SitesLoyalU$SiteCode[9]<-"Fall.Hoagland"
+SitesLoyalU$SiteCode[10]<-"FlagMarsh.Pigeon"
+SitesLoyalU$SiteCode[11]<-"Grandad.Hessler"
+SitesLoyalU$SiteCode[12]<-"Huckle.Loyalsock"
+SitesLoyalU$SiteCode[13]<-"Lake.Elk"
+SitesLoyalU$SiteCode[14]<-"Level.Lick"
+SitesLoyalU$SiteCode[15]<-"Lick.Kings"
+SitesLoyalU$SiteCode[16]<-"Mill.Loyalsock"
+SitesLoyalU$SiteCode[17]<-"Painter.LittleBear"
+SitesLoyalU$SiteCode[18]<-"Porter.Hoagland"
+SitesLoyalU$SiteCode[19]<-"Red.LittleBear"
+SitesLoyalU$SiteCode[20]<-"Rock.LittleLoyalsock"
+SitesLoyalU$SiteCode[21]<-"Sand.Mill"
+SitesLoyalU$SiteCode[22]<-"SandSpring.LittleBear"
+SitesLoyalU$SiteCode[23]<-"Scar.Loyalsock"
+SitesLoyalU$SiteCode[24]<-"Sherman.Loyalsock"
+SitesLoyalU$SiteCode[25]<-"Shingle.Bear"
+SitesLoyalU$SiteCode[26]<-"Snake.Bear"
+SitesLoyalU$SiteCode[27]<-"Streby.Lick"
+SitesLoyalU$SiteCode[28]<-"Swamp.Hoagland"
+SitesLoyalU$SiteCode[29]<-"Weed.Hoagland"
+SitesLoyalU$SiteCode[30]<-"Yellow.LittleLoyalsock"
+SitesLoyalU
+#  SiteCodes match SiteName
+#write_csv(SitesLoyalU,"SitesLoyalU.csv")
+
+#Update SiteCodes in Event df to match Temperature files
 EventsLoyal$SiteCode[1:10]<-"Bear.Loyalsock"
 EventsLoyal$SiteCode[11:20]<-"Brunnerdale.Ogdonia"
 EventsLoyal$SiteCode[21:31]<-"Coal.Loyalsock"
@@ -121,50 +178,12 @@ EventsLoyal$SiteCode[277:286]<-"Swamp.Hoagland"
 EventsLoyal$SiteCode[287:296]<-"Weed.Hoagland"
 EventsLoyal$SiteCode[297:306]<-"Yellow.LittleLoyalsock"
 EventsLoyal
-
-#    Time had only 9 surveys out of 306 with a time recorded - removed from data prep.
-#QC- that all events make sense as LTL.
-EvLoySum<-EventsLoyal%>%
-  group_by(SiteCode)%>%
-  summarise(
-    LoyalEventCount = n()
-    )
-EvLoySum
-# 24 of 30 sites have 10 events.
-# Sites with 11 events need investigated: Coal, DryHB, Granddad, Painter, Red, Rock
-EventsInv<-EventsLoyal%>%
-  filter(SiteCode =="Coal.UpperLoyalsock"|SiteCode =="DryHB.Hoagland"|SiteCode =="Grandad.Hessler"|
-           SiteCode =="Painter.LittleBear"|SiteCode =="Red.LittleBear"|SiteCode =="Rock.LittleLoyalsock")%>%
-  arrange(SiteCode,EventCode)
-EventsInv# all sites surveyed in 2021 - an additional year after all other sites labeled Loyalsock.
-#Create Table for reducting other table to LTL only
-EventsLoyalR<-EventsLoyal[,c(1,2)]
-EventsLoyalR
+#   All sitecodes match with eventcodes
 #write_csv(EventsLoyal,"EventsLoyal.csv")
 
-#Reduce Sites to only Sites categorized as Loyalsock
-SitesLoyal<-left_join(EvLoySum,Sites,by="SiteCode")
-SitesLoyalU<-SitesLoyal[, c(1,4,5,2,6,7)]%>%
-  arrange(SiteCode)
-SitesLoyalU# 30 x 6
-#Update Site Names to match Temperature files
-
-SitesLoyalU$SiteCode[1]<-"Bear.Loyalsock"
-
-SitesLoyalU$SiteCode[3]<-"Coal.Loyalsock"
-
-SitesLoyalU$SiteCode[4]<-"Conklin.Mill"
-
-SitesLoyalU$SiteCode[5]<-"Dry.Hoagland"
-
-SitesLoyalU$SiteCode[6]<-"Dry.Loyalsock"
-
-SitesLoyalU$SiteCode[7]<-"Dutchman.Loyalsock"
-
-SitesLoyalU$SiteCode[8]<-"Ellis.Loyalsock"
-
-SitesLoyalU
-#write_csv(SitesLoyalU,"SitesLoyalU.csv")
+#Create Table for reducing other table to LTL only
+EventsLoyalR<-EventsLoyal[,c(1,2)]
+EventsLoyalR
 ################################################################################
 #Fish Data
 
@@ -277,7 +296,7 @@ FishRecords
 #Try to Reduce records but need to QC to ensure no LTL records are lost.
 FishRecLoy<-left_join(x=EventsLoyalR,y=FishRecords,by="EventCode",multiple="all")
 FishRecLoy  
-# Did a right join so that I could QC for Events when the SiteCode is 'NA'
+#QC for Events when the SiteCode is 'NA'
 QCFishRec<-FishRecLoy%>%
   group_by(SiteCode,EventCode)%>%
   summarize(
@@ -288,14 +307,30 @@ QCFishRec # 306 events - check
 LTLSites<-QCFishRec%>%
   group_by(SiteCode)%>%
   summarise(count = n())
-LTLSites # 3- sites - check
+LTLSites # 30- sites - check
+#QC Species list
+QCSpp<-FishRecLoy%>%
+  select(SiteCode,EventCode,Species)%>%
+  group_by(SiteCode,EventCode,Species)%>%
+  summarize(
+    Count = n()
+  )
+QCSpp
+#   4 rows with no species:
+#       2012 - Ellis; 2013 - Ellis; 2012 - Sand; 2013 - Sherman
 
 #Now reduce fish records to Brook Trout only
 BKTRecLoy<-FishRecLoy%>%
   filter(Species=="Brook Trout")
 BKTRecLoy
-
-
-write.csv(BKTRecLoy,"BKTRecLoy.csv")
+# Issues with TL or Wt:
+#    2012 Huckle: 1BKT, No TL/No Wt
+#    2013 Huckle: 1BKT, No TL/No Wt
+#    2018 Scar:   1BKT, No TL/ Wt
+#    2013 Shingle: 1BKT, No TL/No Wt
+#    2013 Shingle: 18BKT,  TL/No Wt
+#    2012 Yellow: 1BKT,  TL/No Wt
+#  Not technically an issue for 2019-2021, our years of interest for this project.
+#write.csv(BKTRecLoy,"BKTRecLoy.csv")
 ###############################
 #Currently don't need Fish Tally data 
