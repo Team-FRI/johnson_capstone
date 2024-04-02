2499d619c0a4da277be4c1c1c188b54ef8dffe91
tjoejohn <112492318+tjoejohn@users.noreply.github.com>
2024-04-01 22:36:37 -0400

    Night Work 4/1

--- Spring/Modeling.R
+++ Spring/Modeling.R
@@ -17,10 +17,10 @@ JulAugSTpred <- read_csv("JulAugSTPred.csv")
 install.packages("mgcv")
 library(mgcv)
 
-BKTVar <- BKTVar %>%
-  filter(!(Year=="2019"))
+#BKTVar <- BKTVar %>%
+#  filter(!(Year=="2019"))
 
-Data_merge <- merge(BKTVar, STPred, by = c("SiteCode", "Year"), all = FALSE)
+#Data_merge <- merge(BKTVar, STPred, by = c("SiteCode", "Year"), all = FALSE)
 
 #Data_merge_Spring <- merge(BKTVar, MarAprSTPred, by = c("SiteCode"), all = FALSE)
 
@@ -32,12 +32,38 @@ Data_merge_Summer <- filter(Data_merge_Summer,(!(SiteCode=="Brunnerdale.Ogdonia"
            SiteCode=="Sherman.Loyalsock" & Year.x=="2020" & Year.y=="2021"|
              SiteCode=="Shingle.Bear" & Year.x=="2020" & Year.y=="2021")))
 
-Data_merge_CompleteOnly <- merge(BKTVar, STPred_completeOnly, by = c("SiteCode", "Year"), all = FALSE)   
+Data_merge_CompleteOnly <- merge(BKTVar, STPred_completeOnly, by = c("SiteCode", "Year"), all = FALSE)  
+
+#Filter out BKTVAr for 2021
 
 BKTVar2021 <- BKTVar %>%
   filter(Year=="2021")
 
-STPred or ST <- BKTVar %>%
-  filter(Year=="2021")
+#Instal packages to filter STPred for 2020, idk if I need these anymore though. From when I was trying to figure out how to filter STPred for 2020. 
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("lubridate")
+library(lubridate)
+
+#Filter for 2020 and 2021
+
+STPred1 <- STPred %>%
+  filter(Year == "2020" | Year == "2021")
+
+
+
+#STPred2 <- STPred1 %>%
+#  filter(as.numeric(Month) >= 6)
+
+#Next filter to keep all months in 2021 and in 2020 only months that start at June and everything after.
+#Maybe change this to July (07)?
+
+STPred2 <- STPred1 %>%
+  filter((Year == "2021") | (Year == "2020" & as.numeric(Month) >= 6))
+
+Data_merge_Model <- merge(BKTVar2021, STPred1, by = c("SiteCode", "Year"), all = FALSE)
+
 
 #Filter for 6 sites in BKTVar2021!!!!!!!!
